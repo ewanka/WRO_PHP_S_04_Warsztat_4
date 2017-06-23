a5877de8cac1da08ebc0ab067fc0c9cac3864ded
Ewa <nowak.ewka@gmail.com>
2017-06-23 16:11:45 +0200

    add test for constructor

--- tests/TestCustomer.php
+++ tests/TestCustomer.php
@@ -1,6 +1,15 @@
 <?php
 
-Class TestCustomer
-{
+require_once 'src/Customer.php';
 
+Class TestCustomer extends PHPUnit_Framework_TestCase
+{
+    public function testConstruct()
+    {
+        $customer1 = new Customer('nazwa', 'nazwisko', 'email');
+        
+        $this->assertEquals('nazwa', $customer1->getName());
+        $this->assertEquals('nazwisko', $customer1->getSurname());
+        $this->assertEquals('email', $customer1->getEmail());
+   }
 }
