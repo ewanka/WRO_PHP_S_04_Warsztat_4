9f07eb65d4812b9c66ffb1c3a64989b7efec4c7c
Ewa <nowak.ewka@gmail.com>
2017-06-23 17:05:58 +0200

    add test for function login

--- tests/TestCustomer.php
+++ tests/TestCustomer.php
@@ -6,10 +6,22 @@ Class TestCustomer extends PHPUnit_Framework_TestCase
 {
     public function testConstruct()
     {
-        $customer1 = new Customer('nazwa', 'nazwisko', 'email');
+        $customer1 = new Customer('nazwa', 'nazwisko', 'email', 'password');
         
         $this->assertEquals('nazwa', $customer1->getName());
         $this->assertEquals('nazwisko', $customer1->getSurname());
         $this->assertEquals('email', $customer1->getEmail());
+        
+        
+   }
+   
+   public function testLogin()
+   {
+       $customer1 = new Customer('nazwa', 'nazwisko', 'email', 'password', 10);
+       
+       
+       $this->assertEquals('email', $customer1->getEmail());
+       $this->assertTrue($customer1->login('password','email'));
+       
    }
 }
