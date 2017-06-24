9ff412b518f2ee2b17faa20857d01323c8c2ca46
Ewa <nowak.ewka@gmail.com>
2017-06-24 18:36:28 +0200

    edit saveToDb function

--- src/Customer.php
+++ src/Customer.php
@@ -2,6 +2,7 @@
 
 require_once '../MySQLConnection.php';
 require_once 'Admin.php';
+require_once '../web/register.php';
 
 //Stwórz (w katalogu /src) klasę dla użytkownika.
 //
@@ -28,115 +29,48 @@ Class Customer
         $this->password = "";
     }
 
-    public function getName()
-    {
-        return $this->name;
-    }
-
-    public function getSurname()
-    {
-        return $this->surname;
-    }
-
-    public function getEmail()
-    {
-        return $this->email;
-    }
-
-    public function setName($name)
-    {
-        $this->name = $name;
-    }
-
-    public function setSurname($surname)
-    {
-        $this->surname = $surname;
-    }
-
     public function setEmail($email)
     {
         $this->email = $email;
     }
 
-    private function setPassword()
+    public function setPassword($password)
     {
-        $this->password = password_hash($password, PASSWORD_BCRYPT);
+        $this->password = $password;
     }
 
-    private function setShippingAddress()
+    public static function saveToDb(PDO $conn)
     {
-        $this->shippingAddress = $shippingAddress;
-    }
+        
+        if ($_SERVER['REQUEST_METHOD'] === $_POST) {
+            if (isset($_POST["register_submit"]) && (!empty($_POST["register_email"])) && (!empty($_POST["register_passsword"]))) {
+               // $customer = new Customer();
+               $this->setEmail($_POST["register_email"]);
+               $this->setPassword($_POST["register_passsword"]);
+                
+                    if($this->id == -1) {
 
-    protected function showHistoryOfOrders(PDO $conn)
-    {
-        $orders = [];
-        $result = $conn->query('SELECT * FROM `customers` JOIN `orders`
-            ON customers.customer_id=orders.customer_id '); //join where customer_id = ?
-    }
+                    $stmt = $conn->prepare(
+                        'INSERT INTO customers (email, password) VALUES (:email, :password)'
+                     );
+                    $result = $stmt->execute(
+                        ['email' => $this->email, 'password' => $this->password]
+                    );
 
-    public function saveToDB(PDO $conn)
-    {
-        if (!null($_POST === ['REQUEST_METHOD']) && !empty($_POST["register_name"])){
-            
-        if ($this->id == -1) { //ustawić id = -1 w constructor
-            $sql = "INSERT INTO `customers` (name, surname, email, password, shipping_address) VALUES (:name, :surname :email, :password, :shipping_address)";
-
-            $stmt = $conn->prepare($sql);
-
-            $stmt->execute([
-                ':name' => $this->name,
-                ':surname' => $this->surname,
-                ':email' => $this->email,
-                ':password' => $this->password,
-                ':shipping_address' => $this->shippingAddress
-            ]);
-
-            $this->id = $conn->lastInsertId();
-
-            return true;
+                return true;
+                    }
+            }
         }
-        else {
-
-            $sql = "UPDATE customers SET name=:name, surname=:surname, email=:email,
-                    password=:password, shipping_address=:shipping_address WHERE id=:id";
-
-            $stmt = $conn->prepare($sql);
-
-            return $stmt->execute([
-                        ':name' => $this->name,
-                        ':surname' => $this->surname,
-                        ':email' => $this->email,
-                        ':password' => $this->password,
-                        ':shipping_address' => $this->shippingAddress,
-                        ':id' => $this->id,
-            ]);
-        }
-        }  
-    }
-
-    public function login($password, $email)
-    {
-        // connectDB();
-        //if ($_POST === ['REQUEST_METHOD']) {
-        // if (!isset($_POST['password']) && !isset($_POST['email'])) {
-        //echo 'Enter proper username and password';
-        //return false;
-        // } else {
-        //$dbPassword; //= SELECT id FROM `customers` ;
-        //$dbUsername;
-        // if (($_POST['password'] === 'password') && ($_POST['email'] === 'email')) {
-        //  echo 'Logged in'; // przekierowanie do strony...?
-
-        return $email === $this->email && $password === $this->password;
-        // }
-        //}
-        // }
-    }
-
-    public function receiveMessageFromAdmin()
-    {
-        return;
     }
 
 }
+
+var_dump(Customer::saveToDb($conn));
+
+//$newCustomer = new Customer();
+//$newCustomer->setEmail($_POST["register_email"]);
+//$newCustomer->setPassword($_POST["register_passsword"]);
+//$newCustomer::saveToDb($conn);
+//var_dump($newCustomer);
+
+
